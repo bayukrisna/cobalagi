7afef1b96bca2b97b73e179766d6303273d7bca5
Bayu Krisna <bayu_krisna_24rpl@student.smktelkom-mlg.sch.id>
2017-01-06 16:44:11 -0800

    Add project files.

---
+++ Cobalagi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cobalagi.Startup))]
+namespace Cobalagi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
